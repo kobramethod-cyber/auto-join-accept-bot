0e565bba3cdac306a55107b3ce5ac90da517e555
kobramethod-cyber <kobramethod@gmail.com>
2026-09-12 13:54:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -123,8 +123,8 @@ async def join_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.approve()
         add_user_to_db(user.id)
 
-        links = load_links()
-                keyboard = [
+                links = load_links()
+        keyboard = [
             [InlineKeyboardButton(links["b1_t"], url=links["b1_u"])],
             [InlineKeyboardButton(links["b2_t"], url=links["b2_u"])]
         ]
